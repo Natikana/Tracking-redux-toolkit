4a3a077bb95a7960e0d98c7c0270bea3c47fa0b6
natikana <mashkovski82@gmail.com>
2023-06-06 16:30:25 +0200

    Updates

---
+++ precache-manifest.c0b867afb3b03007595fde38ed53c777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3758ca4a6f4d3dced65c0c89f6b3a0fd",
+    "url": "/Tracking-redux-toolkit2/index.html"
+  },
+  {
+    "revision": "c076d5eab85e9c72e611",
+    "url": "/Tracking-redux-toolkit2/static/css/main.e287466b.chunk.css"
+  },
+  {
+    "revision": "98115e95cb312a286aea",
+    "url": "/Tracking-redux-toolkit2/static/js/2.f0356da8.chunk.js"
+  },
+  {
+    "revision": "5826ac4079211d9b741d1ba2b14fe70c",
+    "url": "/Tracking-redux-toolkit2/static/js/2.f0356da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c076d5eab85e9c72e611",
+    "url": "/Tracking-redux-toolkit2/static/js/main.cf76b087.chunk.js"
+  },
+  {
+    "revision": "df556bad5cd44275666f",
+    "url": "/Tracking-redux-toolkit2/static/js/runtime-main.a1840ef3.js"
+  }
+]);
